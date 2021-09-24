5a340e833cbf964605bcb7f96a79ddac92238be2
Shubham Shukla <shubhamshukla@774@gmail.com>
2021-09-25 01:56:04 +0530

    project setup

---
+++ src/main/java/com/MyProject/DailyUse/DailyUseApplication.java
@@ -0,0 +1,13 @@
+package com.MyProject.DailyUse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyUseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyUseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MyProject/DailyUse/DailyUseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MyProject.DailyUse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyUseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
